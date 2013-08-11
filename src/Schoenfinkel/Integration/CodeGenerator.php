c2acfcb3f5f6d6a8e5a98b5ff30d909fc28edf30
Ruben de Gooijer <rubendegooijer@gmail.com>
2013-08-11 19:21:24 +0200

    add license

--- src/Schoenfinkel/Integration/CodeGenerator.php
+++ src/Schoenfinkel/Integration/CodeGenerator.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of the php-partial-constructor package.
+ * (c) Ruben Alexander de Gooijer <rubendegooijer@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Schoenfinkel\Integration;
 
 use \ReflectionClass;
